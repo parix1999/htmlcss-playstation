0f484df7229128a894d03f582d49cee858c8ea50
parix1999 <parisottoedoardo@gmail.com>
2021-05-26 15:32:04 +0200

    Ps5-5.0.0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+alert('Ciao sono javascript e sono al tuo servizio');
+console.log=('alert ciao sono javascript');
+
+// scritta iniziale su PS5
+document.getElementById('new-better').innerHTML='New is always Better';
+console.log=('secondo elemento aggiuntivo ps5');
+
+// scritta su PS5 descrizione prodotto
+document.getElementById('style').innerHTML='Play with style';
+
+
+// scritta su PS4
 document.getElementById('subtitle-ps4').innerHTML='Old but Gold';
-console.log=('meraviglioso');
-alert('Ciao sono javascript al tuo servizio');
+console.log=("elemento aggiuntivo ps5");
